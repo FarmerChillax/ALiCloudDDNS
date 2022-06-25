ab002536d5415a03cf2209a22f29eedb41b6b8e7
FarmerChillax <farmer-chong@qq.com>
2022-06-26 01:38:37 +0800

    init 初始化重构

--- client/client.go
+++ client/client.go
@@ -1,21 +1,49 @@
 package client
 
+import (
+	"log"
+)
+
 type DNSAgent interface {
-	GetIp(string) string
+	GetIp(...string) string
 	Update(string) (bool, error)
 }
 
 type DDNSClient struct {
-	Agent         DNSAgent
-	DnsHostIp     string
-	CurrentHostIp string
+	Agent              DNSAgent
+	GetCurrentIpClient *GetIpClient
+	DnsHostIp          string
 }
 
-func New() *DDNSClient {
+func New(rr string) *DDNSClient {
 	// 当前版本只做阿里云
 	aliAgent := ALiDNSClient{}
+	getIpclient := NewGetIpClient()
 	ddnsClient := DDNSClient{
-		Agent: &aliAgent,
+		Agent:              &aliAgent,
+		GetCurrentIpClient: getIpclient,
+		DnsHostIp:          aliAgent.GetIp(rr),
 	}
 	return &ddnsClient
+}
+
+func (d *DDNSClient) Run() {
+	currentIp, err := d.GetCurrentIpClient.Get()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	if d.DnsHostIp != currentIp {
+		// fmt.Println(d.DnsHostIp, currentIp)
+		// DNS 解析的 IP 与本机不符
+		// 则更新解析 IP
+		ok, err := d.Agent.Update(currentIp)
+		if err != nil {
+			log.Fatalf("[INFO] 更新解析 IP 出错, err: %v\n", err)
+			return
+		}
+		if !ok {
+			log.Println("[INFO] 更新解析 IP 失败")
+		}
+	}
 }
