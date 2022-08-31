c0f1643ce3f4e62d6a99d8b4bab4509211b406bc
FarmerChillax <farmer-chong@qq.com>
2022-08-31 14:11:29 +0800

    chore: 构建心跳

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"log"
 	"net"
 
-	"github.com/FarmerChillax/ALiCloudDDNS/log"
+	"github.com/FarmerChillax/ALiCloudDDNS/proto/ddns_server"
+	"github.com/FarmerChillax/ALiCloudDDNS/server"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 var address = "127.0.0.1:5000"
@@ -15,7 +18,16 @@ var serverCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		lis, err := net.Listen("tcp", address)
 		if err != nil {
-			log.FileLog
+			// log.FileLog
+		}
+
+		grpcServer := grpc.NewServer()
+
+		ddns_server.RegisterDdnsServerServer(grpcServer, server.NewDdnsServer())
+
+		log.Printf("starting gRPC listener on port: %v", address)
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
 		}
 	},
 }
--- server/server.go
+++ server/server.go
@@ -1,8 +1,32 @@
 package server
 
+import (
+	"context"
+	"log"
+
+	"github.com/FarmerChillax/ALiCloudDDNS/proto/ddns_server"
+	"google.golang.org/grpc/peer"
+)
+
 type DdnsServer struct {
 }
 
 func NewDdnsServer() *DdnsServer {
 	return &DdnsServer{}
+}
+
+func (ds *DdnsServer) HeartBeatServer(stream ddns_server.DdnsServer_HeartBeatServerServer) error {
+	for {
+		in, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		log.Printf("HeartBeat uuid: %s", in.GetUuid())
+		peer, ok := peer.FromContext(context.Background())
+		// if ok {
+		// 	log.Println(peer, ok)
+		// }
+		log.Println(peer, ok)
+	}
+	return nil
 }
